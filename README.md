89bb0b4f5ae3ba96d64b5ff87da23b6adcba3de8
chelle <maphaiyarath@gmail.com>
2020-10-26 05:23:53 -0500

    more mobile friendly ui and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# work-day-scheduler
+# Third-Party APIs: Work Day Scheduler
+
+You can see the deployed app [here](https://maphaiyarath.github.io/work-day-scheduler).
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Badges](#badges)
+
+## Description
+
+## Installation
+This project uses [Moment.js](https://momentjs.com/). Install it by running the following command:
+```
+npm install moment
+```
+
+## Usage
+n/a
+
+## Credits
+n/a
+
+## License
+This project is licensed under the MIT license.
+
+## Badges
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/maphaiyarath/work-day-scheduler)
--- script.js
+++ script.js
@@ -21,3 +21,86 @@ timeBlocks.each(function() {
 // WHEN I click the save button for that time block THEN the text for that event is saved in local storage
 
 // WHEN I refresh the page THEN the saved events persist
+
+/*
+
+if (scoreForm) {
+    // when the game is over, the user can save their initials and score
+    scoreForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+    
+        var initialText = initialInput.value.trim();
+    
+        // do not accept empty initials
+        if (initialText === '') {
+            return;
+        }
+
+        var userScore = {
+            'initials': initialText,
+            'score': score
+        };
+        
+        // add the new score to the scores array
+        scores.push(userScore);
+        
+        // reset the input field
+        initialInput.value = '';
+    
+        // add the scores array to localStorage
+        localStorage.setItem('scores', JSON.stringify(scores));
+
+        // go to highscores page
+        window.location.href = "https://maphaiyarath.github.io/code-quiz/highscores.html";
+    });
+    
+}
+
+// grab the previous high scores from localStorage
+function init() {
+    var storedScores = JSON.parse(localStorage.getItem("scores"));
+
+    if (storedScores !== null) {
+        scores = storedScores;
+    }
+}
+
+// present the high scores as a table
+function showScores() {
+    var storedScores = JSON.parse(localStorage.getItem("scores"));
+
+    if (storedScores !== null) {
+        scores = storedScores;
+    }
+
+    // create a row for each score found
+    for (var i = 0; i < scores.length; i++) {
+        var scoreItem = scores[i];
+
+        var tr = document.createElement("tr");
+        scoresEl.append(tr);
+        
+        var tdInitial = document.createElement("td");
+        tdInitial.textContent = scoreItem.initials;
+        tr.append(tdInitial);
+
+        var tdScore = document.createElement("td");
+        tdScore.textContent = scoreItem.score;
+        tr.append(tdScore);
+    }
+}
+
+// self-explanatory
+function clearScores() {
+    scoresEl.innerHTML = '';
+    localStorage.clear();
+}
+
+// if on high scores page, then show scores
+if (scoresEl) {
+    showScores();
+}
+
+init();
+
+*/
