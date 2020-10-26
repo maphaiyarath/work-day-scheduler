57de80019ed62b376c19785521293388b57f2b07
chelle <maphaiyarath@gmail.com>
2020-10-26 06:10:24 -0500

    able to save event to localStorage

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+var currentHour = moment().format("HH");
+var timeBlocks = $(".time-block");
+var saveButton = $(".saveBtn");
+var events = [];
+
 // WHEN I open the planner THEN the current day is displayed at the top of the calendar
 $("#currentDay").html(moment().format("dddd, MMMM Do,  YYYY"));
 
 // WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
-var currentHour = moment().format("HH");
-var timeBlocks = $(".time-block");
-
 timeBlocks.each(function() {
     var blockHour = $(this).data("hour");
     var blockDes = $(this).children(".description");
@@ -19,61 +21,46 @@ timeBlocks.each(function() {
 });
 
 // WHEN I click the save button for that time block THEN the text for that event is saved in local storage
+saveButton.click(function() {
+    var blockHour = $(this).parent('.time-block').data("hour");
+    var desText = $.trim($(this).siblings('.description').val());
+    console.log(desText);
+
+    if (desText === '') {
+        return;
+    }
+
+    var des = {
+        'hour': blockHour,
+        'event': desText
+    };
+
+    events.push(des);
+
+    localStorage.setItem('events', JSON.stringify(events));
+});
 
 // WHEN I refresh the page THEN the saved events persist
-
-/*
-
-if (scoreForm) {
-    // when the game is over, the user can save their initials and score
-    scoreForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-    
-        var initialText = initialInput.value.trim();
-    
-        // do not accept empty initials
-        if (initialText === '') {
-            return;
-        }
-
-        var userScore = {
-            'initials': initialText,
-            'score': score
-        };
-        
-        // add the new score to the scores array
-        scores.push(userScore);
-        
-        // reset the input field
-        initialInput.value = '';
-    
-        // add the scores array to localStorage
-        localStorage.setItem('scores', JSON.stringify(scores));
-
-        // go to highscores page
-        window.location.href = "https://maphaiyarath.github.io/code-quiz/highscores.html";
-    });
-    
-}
-
-// grab the previous high scores from localStorage
 function init() {
-    var storedScores = JSON.parse(localStorage.getItem("scores"));
+    var storedEvents = JSON.parse(localStorage.getItem('events'));
 
-    if (storedScores !== null) {
-        scores = storedScores;
+    if (storedEvents !== null) {
+        events = storedEvents;
+    }
+};
+
+function showEvents() {
+    var storedEvents = JSON.parse(localStorage.getItem('events'));
+
+    if (storedEvents !== null) {
+        events = storedEvents;
     }
 }
 
-// present the high scores as a table
-function showScores() {
-    var storedScores = JSON.parse(localStorage.getItem("scores"));
+init();
+showEvents();
 
-    if (storedScores !== null) {
-        scores = storedScores;
-    }
-
-    // create a row for each score found
+/* function showScores() {
     for (var i = 0; i < scores.length; i++) {
         var scoreItem = scores[i];
 
@@ -88,19 +75,4 @@ function showScores() {
         tdScore.textContent = scoreItem.score;
         tr.append(tdScore);
     }
-}
-
-// self-explanatory
-function clearScores() {
-    scoresEl.innerHTML = '';
-    localStorage.clear();
-}
-
-// if on high scores page, then show scores
-if (scoresEl) {
-    showScores();
-}
-
-init();
-
-*/
+} */
